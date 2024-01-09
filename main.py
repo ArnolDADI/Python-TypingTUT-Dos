2359843cd33eada89007d9055ef21833c2f5e72e
ArnolDADI <arnoldredcliffe@gmail.com>
2024-01-09 16:47:54 +0530

    refurbished

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from time import time
 class Tutorial:
     def __init__(self) -> None:
         """Initiating the name variable and empty profile list"""
-        self.name = ''
-        self.profiles = []
+        self.name       = ''
+        self.profiles   = []
 
         # Lambda function to clear screen
         self.cls = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
 
     def run(self):
         '''Initiate the function for the first time'''
@@ -373,99 +374,116 @@ class checkWord(subMenu):
     def check_word(self, text, name):
         """ To check correctness of the typing tutorial while using """
         #variables
-        count = 0
-        count_letter = 0
-        count_word = 0
+        lengthText      = len(text)
+        lenDisp         = int((os.popen('tput cols')).read())      # To measure the width of the terminal
+        lenHeight         = int((os.popen('tput lines')).read())//2      # To measure the width of the terminal
+        lenTypedDisp    = lenDisp//2
+        listWord        = list(text.split())
+        lenListW        = len(listWord)
+
+        count       = 0
+        countLetter = 0
+        countWord   = 0
+        countLRight = 0 
+        countWRight = 0 
+        countLWrong = 0 
+        countWWrong = 0 
+
+        typedL      = ''
+        typedW      = ''
+        inputL      = ''
+        inputLUF    = ''
+        string      = ''
+
+        startTime = time()      # Taking the starting time of the test
+
         print(text)
-        list_word = list(text.split())      # Create a list of the words in the text
-        count_right = 0
-        count_wrong = 0
-        typed_letter = ''
-        typed_word = ''
-        input_letter = ''
-        str = ''
-        start = time()
-        input_letter_unformatted = ''
-        
-        #loop to take input and check it
-        while count_word < len(list_word):
-            
+
+        while countWord < lenListW:
+             
             counter = 0
             flag = 0
-
             # To add space after the word except the last one
-            if count_word != len(list_word) -1:
-                typed_word = list_word[count_word] + ' '
+            if countWord != lenListW -1:
+                typedW = listWord[countWord] + ' '
             else:
-                typed_word = list_word[count_word]
-
-            count_letter = len(typed_word)
-
-            # To check if the typed word is same as the inputted word
-            while counter < count_letter:
+                typedW = listWord[countWord]
+            countLetter = len(typedW)
+            # To check if the typed word is same as the inputed word
+            while counter < countLetter:
                 
-                key = getch()
-                input_letter_unformatted += key
+                key = getch()      # Get the key press from the user
+                self.cls()
 
+                print('\n' * lenHeight)
+                inputLUF += key
                 # To make a string for the typed words
-                if key == typed_word[counter]:
-                    typed_letter += f"{color['green']}{typed_word[counter]}"
-                    input_letter += f"{color['green']}{key}"
+                if key == typedW[counter]:
+                    typedL += f"{color['green']}{typedW[counter]}"
+                    inputL += f"{color['green']}{key}"
                 else:
+                    print('\a', end = '\r')
                     flag = 1
-                    typed_letter += f"{color['bright_red']}{typed_word[counter]}{color['green']}"
-                    input_letter += f"{color['bright_red']}{key}{color['green']}"
+                    typedL += f"{color['bright_red']}{typedW[counter]}{color['green']}"
+                    inputL += f"{color['bright_red']}{key}{color['green']}"
 
-                self.cls()       # Clear screen
+#                 self.cls()       # Clear screen
 
-                # To print the string 
-                try:
-                    print(f"{typed_letter}{color['bright_blue']}{color['bold']}{color['bg_white']}\
-{text[count+1]}{color['reset']}{color['bright_yellow']}{text[count+2:]}{color['reset']}")
-                except:
-                    print(f"{typed_letter}")
+#               # To print the string 
                 
+                if count < lenTypedDisp:
+                    print(f"{typedL}{color['bright_blue']}{color['bold']}{color['bg_white']}{text[count+1]}{color['reset']}{color['bright_yellow']}{text[count+2:count+lenTypedDisp]}{color['reset']}")
+                elif count >= lenTypedDisp and lengthText - count > lenTypedDisp -1:
+                    print(f"{typedL[-lenTypedDisp-2:]}{color['bright_blue']}{color['bold']}{color['bg_white']}{text[count+1]}{color['reset']}{color['bright_yellow']}{text[count+2:count+2+lenTypedDisp-1]}{color['reset']}")
+                elif lengthText - count < lengthText - lenTypedDisp -1:
+                    try:
+                        print(f"{typedL[-lenTypedDisp-2:]}{color['bright_blue']}{color['bold']}{color['bg_white']}{text[count+1]}{color['reset']}{color['bright_yellow']}{text[count+2:]}{color['reset']}")
+                    except IndexError:
+                        print(f"{typedL}FOURTH")
+                    
                 counter += 1
                 count += 1
 
-            try:
-                str += typed_letter
-            except:
-                str += typed_letter 
+            # try:
+            #     string += typedL
+            # except:
+            #     string += typedL 
+            string += typedL 
 
             if flag == 1:
-                count_wrong += 1
+                countWWrong += 1
             else:
-                count_right += 1
+                countWRight += 1
 
-            count_word += 1
+            countWord += 1
 
-        end  = time()
+        endTime  = time()       # Taking the end time of the test
 
-        print(f"{input_letter}{color['reset']}")
+        print(f"{inputL}{color['reset']}")
 
-        time_taken = end - start
-        accuracy = (count_right/count_word)*100
-        wpm = count_word/(time_taken/60)
-        cpm = count/(time_taken/60)
+        timeTaken = endTime - startTime
+        accuracy = (countWRight/countWord)*100
+        WPM = countWord/(timeTaken/60)
+        CPM = count/(timeTaken/60)
 
         # To print the results and parameters of the test
-        print(f">>>Total words = {count_word}\n>>>Correct words = {count_right}\n>>>Wrong words = {count_wrong}")
-        print(f">>>Accuracy = {accuracy}%")
-        print(f">>>Time taken = {time_taken}")
+        print('-'*lenDisp)
+        print(f">>>Total words = {countWord}\n>>>Correct words = {countWRight}\n>>>Wrong words = {countWWrong}")
+        print(f">>>Accuracy = {accuracy:.2f}%")
+        print(f">>>Time taken = {timeTaken:.2f}")
         print(f">>>Total keys pressed = {count}")
-        print(f">>>cpm = {cpm}")
-        print(f">>>wpm = {wpm}")
+        print(f">>>CPM = {CPM:.2f}")
+        print(f">>>WPM = {WPM:.2f}")
+        print('-'*lenDisp)
 
-        list_details = [text, input_letter_unformatted, count, count_word, count_right, count_wrong, int(accuracy), int(time_taken), int(cpm), int(wpm)]
 
-        self.details(list_details, name)
+        dictDetails = {'text' : text, 'inputLUF' : inputLUF, 'count' : count, 'countWord' : countWord, 'countWRight' : countWRight, 'countWWrong' : countWWrong, 'accuracy' : int(accuracy),'timeTaken' : int(timeTaken), 'CPM' : int(CPM), 'WPM' : int(WPM)}
 
+        self.details(dictDetails, name)
         # To return different values from the result of the test to the parent file
-        if accuracy >= 80 and wpm >= 33:
+        if accuracy >= 80 and WPM >= 33:
             print("PASSED")
-            return 1
-        
+            return 1      
         else:
             print("FAIL")
             return 0
@@ -476,116 +494,33 @@ class checkWord(subMenu):
         """To store the details and log the tuts
         manages log_10 file, data_all file and data_typed_text"""
 
-        raw_text = result[0]
-        input_text = result[1]
+        raw_text = result['text']
+        input_text = result['inputLUF']
 
     #_________________________________________________________________________________________________________________________________
         # Making and reading a file to read and store details of last ten attempts
-        try:
-            log_10 = open(f'{name}/log_10', 'r')
-        except FileNotFoundError:
-            log_10 = open(f'{name}/log_10', 'a+')
-            log_10.write(f"0 0 0 0 0 0 0 0 0 0")
-            
-        log_10.seek(0)
-        log_10_list = log_10.readlines()
-        log_10.close()
-        log_10 = open(f'{name}/log_10', 'w')
-        try:
-            log_10_list.pop(10)
-        except IndexError:
-            print(">>>size less than 10")
         
-        for i in range (len(log_10_list)):
-            
-            text_list = list(log_10_list[i].split())
-            text_list[0] = int(text_list[0])
-            text_list[0] += 1
-            log_10_list[i] = f"{text_list[0]} {text_list[1]} {text_list[2]} {text_list[3]} {text_list[4]} {text_list[5]}\
-    {text_list[6]} {text_list[7]} {text_list[8]}\n"
-        log_10_list.insert(0, f"1 {result[2]} {result[3]} {result[4]} {result[5]} {result[6]} {result[7]} {result[8]} {result[9]}\n")
-                    
-        log_10.writelines(log_10_list)
+        with open(f'{name}/test.log', 'a+') as testLog:
+            testLog.seek(0)
+            if testLog.read(10) == '':
+                testLog.write("0 0 0 0 0 0 0 0 0\n")
+            testLog.seek(0)
+            logList = testLog.readlines()
+            try:
+                logList.pop(100)
+                print(">>>Popped the last entry")
+            except IndexError:
+                print(">>>Log size currently less than 100")
+
+            testLog.seek(0)
+            testLog.write(f"{result['count']} {result['countWord']} {result['countWRight']} {result['countWWrong']} {result['accuracy']} {result['timeTaken']} {result['CPM']} {result['WPM']}\n")
 
         #_____________________________________________________________________________________________________________________________
-        try:
-            data_all = open(f'{name}/data', 'r')
-            
-        except FileNotFoundError:
-            data_all = open(f'{name}/data', 'a+')
-            data_all.write(f"CTY 0\nCTW 0\nCTR 0\nCTI 0\nACC 0\nTTN 0\nCPM 0\nWPM 0")
+        
 
-        data_all.seek(0)
-        data_all_list = data_all.readlines()
-        data_all.close()
-
-        for i in range (len(data_all_list)):
-            read = data_all_list[i]
-            read_list = list(read.split())
-            if i < 4:
-                read_list[1] = int(read_list[1]) +result[i+2]
-                data_all_list[i] = f"{read_list[0]} {read_list[1]}\n"
-            else:
-                res = 0
-                for line in log_10_list:
-                    read_list = list(line.split())
-                    res += float(read_list[i+1])
-                res /= len(log_10_list)
-                read_list[1] = res
-                data_all_list[i] = f"{read_list[0]} {read_list[1]}\n"
-
-        data_all = open(f'{name}/data', 'w')
-        data_all.writelines(data_all_list)
-
-        #______________________________________________________________________________
-        # detals pf the typed characters
-        try:
-            data_typed_text = open(f'{name}/data_text.txt', 'r+')
-
-        except FileNotFoundError:
-            data_typed_text = open(f'{name}/data_text.txt', 'a+')
-
-            for i in range(33,127):
-                data_typed_text.write(f"{chr(i)} 0 0\n")
-
-            data_typed_text.close()
-            data_typed_text = open(f'{name}/data_text.txt', 'r+')
-
-        data_typed_text.seek(0)
-        count_list = data_typed_text.readlines()
-
-
-        # add total no. of typed characters to the file data_typed_text
-        for i in input_text:
-            for j in range (len(count_list)):
-                read = count_list[j]
-                if i == read[0]:
-                    read_list = list(read.split(' '))
-                    read_list[1] = int(read_list[1])+1
-                    count_list[j] = f"{read_list[0]} {read_list[1]} {read_list[2]}"
-
-        # add total no. wrong characters to the file data_typed_text
-        for i in  range (len(raw_text)):
-            if raw_text[i] != input_text[i]:
-                for j in range (len(count_list)):
-                    read = count_list[j]
-                    if raw_text[i] == read[0]:
-                        read_list = list(read.split(' '))
-                        read_list[2] = int(read_list[2])+1
-                        count_list[j] = f"{read_list[0]} {read_list[1]} {read_list[2]}"
-
-        for i in range(len(count_list)):
-            if count_list[i][-1] != '\n':
-                count_list[i] += '\n'
-
-        data_typed_text.close()
-        data_typed_text = open(f'{name}/data_text.txt', 'w')
-        data_typed_text.seek(0)
-        data_typed_text.writelines(count_list)
-                
     
 
-"""A file to control colour of the text"""
+"""A dictionary to control colour of the text"""
 
 color = {
   'reset': '\x1b[0m',
